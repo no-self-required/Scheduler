4fe13c26a15b61fb7f494363b529c87f9539c0ab
no-self-required <louism.coding@gmail.com>
2021-09-03 22:24:13 -0400

    start hook test

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,16 +4,31 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview} from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {},
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+  
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
+  
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
 
   useEffect(() => {
@@ -27,6 +42,7 @@ export default function Application(props) {
         ...prev,
         days: all[0].data,
         appointments: all[1].data,
+        interviewers: all[2].data,
       }));
     });
   }, []);
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,22 @@
 export function getAppointmentsForDay(state, day) {
   //... returns an array of appointments for that day
-  let results = []
-  for (const item of state.days) {   
+  let results = [];
+  for (const item of state.days) {
     if (item.name === day) {
       for (const x of item.appointments) {
-        results.push(state.appointments[x])
+        results.push(state.appointments[x]);
       }
     }
   }
-  return results
+  return results;
 }
 
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  return {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer],
+  };
+}
