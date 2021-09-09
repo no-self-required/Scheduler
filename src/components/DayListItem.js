b0cab14264c94fb478fbc5dfce9e964b890ccaa7
no-self-required <louism.coding@gmail.com>
2021-09-09 00:46:21 -0400

    add appointment cypress tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -24,10 +24,11 @@ export default function DayListItem(props) {
     "day-list__item": true,
     "day-list__item--selected": props.selected === true,
     "day-list__item--full": props.spots === 0,
+   
   });
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
